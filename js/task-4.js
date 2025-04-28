6456a9b1f6784e12f0c21d455dbebcf24707f0db
Yulia Bezsmertna <yuliabezsmertna80@gmail.com>
2025-04-28 18:53:23 +0300

    correct hm5

--- js/task-4.js
+++ js/task-4.js
@@ -1,6 +1,8 @@
-const getTotalBalanceByGender = (users, gender) => {
+const getTotalBalanceByGender = (users, gender) => users
+.filter(user => user.gender === gender)
+.map(user => user.balance)
+.reduce((total, balance) => total + balance, 0);
 
-}
 const clients = [
 	{
     name: "Moore Hensley",
